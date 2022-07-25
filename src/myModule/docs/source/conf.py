80a5649cf86a37878411bf87432a81c16cd93152
Keitaro Murakami <keitaro.mrkm@gmail.com>
2022-07-26 04:30:08 +0900

    <fix> rel path, not pip

--- src/myModule/docs/source/conf.py
+++ src/myModule/docs/source/conf.py
@@ -26,3 +26,8 @@ exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join('..', '..', '..')))
--- src/myModule/utils.py
+++ src/myModule/utils.py
@@ -9,3 +9,15 @@ def add(x, y):
         int: addition of x and y
     """
     return x + y
+
+def sub(x, y):
+    """_summary_
+
+    Args:
+        x (int): input x
+        y (int): input y
+
+    Returns:
+        int: subtraction of x and y
+    """
+    return x - y
